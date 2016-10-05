22a14ea59890e4f8f55671f260cb834cfe639148
MattyChance <chenmatty.wang@gmail.com>
2016-10-05 18:55:39 +0000

    added function 2 that sorts by name or email

--- functions.js
+++ functions.js
@@ -9,3 +9,30 @@ function sortArr(arr) {
 var arrTest = ['YOU', 'ME', 'BANANA', 'MARVELOUS'];
 
 console.log(sortArr(arrTest));
+
+//function 2: Object name and email
+var arrContact = [
+    {name: 'Matty', email: 'matty@gmail.com'},
+    {name: 'Dog', email: 'dog@gmail.com'},
+    {name: 'honeybadger', email: 'honeybadger@gmail.com'}
+];
+function sortByName (arr) {
+    arr.sort(function (name1, name2) {
+        return name2.name.length - name1.name.length;
+    })
+    return arr;
+}
+
+function sortByEmail (arr) {
+   return arr.sort(function(email1, email2) {
+       if(email1.email > email2.email) {
+         return 1
+       } else if (email1.email < email2.email) {
+         return -1
+       } else {
+         return 0;
+       }
+    })
+}
+
+//function 3: 
